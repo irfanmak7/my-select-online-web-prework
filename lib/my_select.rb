b3542180faa92cbb41e72d15415d31a53b823127
Irfan Maknojia <irfan.mak7@gmail.com>
2018-11-19 21:14:40 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -4,7 +4,7 @@ def my_select(collection)
  new_array = []
  while i < collection.length
    if yield(collection[i]) == true
-     new_array << array[i]
+     new_array << collection[i]
    else
      #nothing
    end
